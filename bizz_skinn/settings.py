1e644c78ee7df9a54b3566847c357e01568d4120
NurKhoirunnisaSalsabila <nur.khoirunnisa@ui.ac.id>
2024-09-24 14:33:03 +0700

    hai lagi

--- bizz_skinn/settings.py
+++ bizz_skinn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-do$bwdk%a=iyoqw!ty(1sr6xt&i#$)386e59&&0^7%rh@%s^61
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nur-khoirunnisa-bizzskinn.pbp.cs.ui.ac.id"]
 
 
 # Application definition
